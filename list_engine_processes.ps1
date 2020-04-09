8b64394ac36677b1e8ef100358f5470882f2b270
Kubiaczyk <michael.kubiaczyk@sap.com>
2020-04-09 16:09:49 +0200

    Better output and checking log lastupdate

--- list_engine_processes.ps1
+++ list_engine_processes.ps1
@@ -18,11 +18,38 @@ foreach ( $engine in $engines ) {
     foreach ( $proc in $processes ) {
         if ( $proc.name -eq "CxEngineAgent.exe" ) {
             $totalProcCount++;
-            $taskID = [regex]::match( $proc.CommandLine, '.*.exe" (\d+)_.*' ).Groups[1].Value
-            $sourceID = [regex]::match( $proc.CommandLine, '.*.exe" \d+_(.{36})' ).Groups[1].Value
+            $match = [regex]::match( $proc.CommandLine, '.*.exe" (\d+)_(.{36})_(.{4})' )
+            $taskID = $match.Groups[1].Value
+            $sourceID = $match.Groups[2].Value
+            $nonce = $match.Groups[3].Value 
             #Write-Host $proc.Commandline " -> $taskID, $sourceID"
 
-            Write-Host `t"Scanning: task $taskID, source $sourceID"
+            
+            $logPath = "\\" + $rhost + "\d$\Program Files\Checkmarx\Checkmarx Engine Server\Engine Server\Scans\" + $taskID + "_" + $sourceID + "_" + $nonce + "\Logs\*"
+
+            
+            $lastModified = 0
+            $created = 0
+            $logup = ""
+
+            Get-ChildItem -Path $logPath | Foreach-Object {
+                $lastModified = $_.LastWriteTimeUtc
+                $created = $_.CreationTimeUtc
+                $diff = New-TimeSpan -Start $lastModified -End (GET-DATE).ToUniversalTime()
+                
+                if ( $diff.TotalHours -ge 24 ) {
+                    $logup = "" + [math]::Round($diff.TotalHours / 24,1) + " days ago"
+                } elseif( $diff.TotalMinutes -ge 60 ) {
+                    $logup = "" + [math]::Round($diff.TotalMinutes / 60, 1) + " hours ago"
+                } else {
+                    $logup = "" + [math]::Round($diff.TotalMinutes, 1 ) + " minutes ago"
+                }
+                
+                
+            }
+
+            Write-Host `t"Scanning: task $taskID source $sourceID - log last updated $lastModified ($logup), created $created"
+
         } else {
             $mem = [int]($proc.WorkingSetSize / (1024*1024));
         }
